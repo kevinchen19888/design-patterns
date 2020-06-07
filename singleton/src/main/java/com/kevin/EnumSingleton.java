229658bd560b3f3d0710fb3fa77e2042f9b77fed
kevinchen19888 <kevinchen19888@163.com>
2020-06-08 00:10:59 +0800

    单例模式添加

---
+++ singleton/src/main/java/com/kevin/EnumSingleton.java
@@ -0,0 +1,18 @@
+package com.kevin;
+
+/**
+ * 基于枚举实现的单例模式
+ * 单元素的枚举类型已经成为实现Singleton的最佳方法 --出自《effective java》
+ */
+public enum EnumSingleton {
+    INSTANCE;
+
+    public EnumSingleton getInstance() {
+        return INSTANCE;
+    }
+
+    public void doSomething() {
+    }
+
+
+}
---
+++ singleton/src/main/java/com/kevin/InnerClassSingleton.java
@@ -0,0 +1,19 @@
+package com.kevin;
+
+/**
+ * 基于静态内部类实现的单例模式
+ * 相较DCL方式和饿汉方式更好:没有进行同步控制,实现了懒加载
+ */
+public class InnerClassSingleton {
+
+    private static class HolderClass {
+        private static final InnerClassSingleton INSTANCE = new InnerClassSingleton();
+    }
+
+    public static InnerClassSingleton getInstance() {
+        return HolderClass.INSTANCE;
+    }
+
+    private InnerClassSingleton() {
+    }
+}
